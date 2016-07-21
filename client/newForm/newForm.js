dca0c7dd95f6ad138ac794ce719ba76d3061b8c9
Mackie <mac.ira353@gmail.com>
2016-07-21 12:10:55 -0400

    added javascript newForm

---
+++ client/newForm/newForm.js
@@ -0,0 +1,17 @@
+import {Template} from 'meteor/templating';
+import './newForm.html'
+
+Template.newForm.events({
+	'submit form': (event, template) => {
+		event.preventDefault();
+
+		let firstName = template.find("#firstName").value;
+		let lastName = template.find("#lastName").value;
+		let quote = template.find("#quote").value;
+		let gender = template.find("#gender").value;
+		let githubLink = template.find("#githubLink").value;
+
+		console.log(firstName);
+	}
+
+})
